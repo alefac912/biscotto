3939e0408ea6d0e45530f1d21424fb34affbd4c4
Alessandra Facchin <facchin.alessandra@gmail.com>
2026-01-14 20:16:35 +0100

    begin

---
+++ sketch.js
@@ -0,0 +1,97 @@
+let table;
+let phrases = [];
+let state = 0;
+let currentPhrase = "";
+
+let paperWidth = 600;
+let paperHeight = 200;
+let paperY, paperX;
+
+let imgWhole, imgLeft, imgRight;
+
+function preload() {
+  table = loadTable("assets/phrases.csv", "csv", "header");
+
+  imgWhole = loadImage('assets/cookie_whole.png');
+  imgLeft = loadImage('assets/cookie_left.png');
+  imgRight = loadImage('assets/cookie_right.png');
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  fill(255, 200, 0);
+  noStroke();
+
+  textFont("Comic Sans MS");
+  textAlign(CENTER, CENTER);
+
+  rectMode(CENTER);
+  ellipseMode(CENTER);
+
+  imageMode(CENTER);
+
+  for (let r = 0; r < table.getRowCount(); r++) {
+    let frase = table.getString(r, "frase");
+    if (frase && frase.length > 0) {
+      phrases.push(frase);
+    }
+  }
+
+  paperY = height / 2;
+  paperX = width / 2;
+}
+
+function draw() {
+  background(0, 0, 255);
+
+  text("ACCookie", width/2, height/2 -350);
+
+  if (state === 0) {
+    //fill(255, 200, 0);
+    //ellipse(width / 2, height / 2, 300);
+
+    image(imgWhole, width/2, height/2);
+
+    fill(255);
+    textSize(24);
+    text("Clicca per aprire il biscotto", width / 2, height / 2 + 200);
+  } else if (state === 1) {
+    //fill(255, 200, 0);
+    //arc(width / 2 - 150, height / 2, 300, 300, HALF_PI, PI + HALF_PI, CHORD);
+    //arc(width / 2 + 150, height / 2, 300, 300, PI + HALF_PI, HALF_PI, CHORD);
+
+    image(imgLeft, width / 2 - 300, height/2);
+    image(imgRight, width / 2 + 300, height/2);
+
+    fill(255);
+    rect(paperX, paperY, paperWidth, paperHeight);
+
+    fill(0);
+    textSize(24);
+    textWrap(WORD);
+    text(currentPhrase, width / 2, height / 2, paperWidth - 50);
+
+    fill(255);
+    textSize(24);
+    text("Clicca per aprire un nuovo biscotto", width / 2, height / 2 + 200);
+  }
+}
+
+function mousePressed() {
+  if (state === 0) {
+    crackCookie();
+  } else {
+    state = 0;
+  }
+}
+
+function crackCookie() {
+  if (phrases.length > 0) {
+    let randomIndex = floor(random(phrases.length));
+    currentPhrase = phrases[randomIndex];
+  } else {
+    currentPhrase = "Nessuna frase trovata nel CSV!";
+  }
+  state = 1;
+}
